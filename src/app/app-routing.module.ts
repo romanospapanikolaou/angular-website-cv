f62f72de9c90c0ffd2170fcc1122cdd4ba9151b2
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-05-30 11:18:13 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorksComponent } from './works/works.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'works', component: WorksComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to HomeComponent by default
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }, // Wildcard route for handling undefined routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
